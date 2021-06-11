80dea6faa86effc0ecabfde1c3a28332dcd17f64
Mark Stevens <markspstevens@yahoo.co.uk>
2021-06-11 09:23:21 +0200

    General Layout

--- scripts.js
+++ scripts.js
@@ -19,28 +19,38 @@ const CreatePage = (photographers) => {
   let photographerhtml = '';
 
   photographers.forEach((person) => {
-    console.log(person);
-    const tagArray = person.tags;
-    console.log(tagArray);
+    /* Using DESTRUCTERING get just the tag array from photographers array */
+    const { tags } = person;
     photographerhtml += `
     <article class="profile">
       <a href="#"><img class="profile__portrait" src="/public/images/photography/Photographers_id_photos/${person.portrait}"
       alt="${person.name}">
       <h2 class="profile__name">${person.name}</h2></a>
-        <p class="profile__address">${person.city}, ${person.country}</p>
-        <p class="profile__tagline">${person.tagline}</p>
-        <p class="profile__price">$ ${person.price} /jour</p>
-        <p class="profile__price">${person.tags[0]} ${person.tags[1]} ${person.tags[2]} ${person.tags[3]}</p>
-        <ul class="profile__tags"></ul>
+        <h3 class="profile__address">${person.city}, ${person.country}</h3>
+        <h4 class="profile__tagline">${person.tagline}</h4>
+        <h5 class="profile__price">${person.price}€/jour</h5>
+        <ul class="profile__tags" >${tags.map((tag) => `<li class="tags">#${tag}</li>`).join('')}</ul> 
     </article>`;
+    photographerElement.innerHTML = photographerhtml;
+    /* .map & .join etc solution found on the internet https://www.javascripttutorial.net/es6/javascript-template-literals/ */
   });
-  photographerElement.innerHTML = photographerhtml;
 };
 
-/*
- const tagElements = document.querySelector('.profile__tags');
-  const tagsArray = person.tags;
-  tagsArray.forEach((tag)=>{
-    let tagList = '';
-    taglist +=`<li>${tag}</li>`;
-  }); */
+/* TEST FUNCTIONS FOR TRYING TO ADD EVENT LISTENER FOR ATG FILTERING */
+
+const TestFunction = (el) => {
+  if (el.classList.contains('test')) {
+    /* el.parentElement.parentElement.remove(); */
+    console.log('Hello');
+  }
+};
+
+document.querySelector('.header__nav').addEventListener('click', (el) => {
+  TestFunction(el.target);
+  console.log(el.target);
+});
+
+document.querySelector('.photographers-container ').addEventListener('click', (e) => {
+  TestFunction(e.target);
+  console.log(e.target);
+});
