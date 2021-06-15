1583ebc8d162724b7db3f58d1c251130061dcfa9
Mark Stevens <markspstevens@yahoo.co.uk>
2021-06-15 13:27:51 +0200

    photoprapher profile responsive

--- js/photographerpage.js
+++ js/photographerpage.js
@@ -1,7 +1,9 @@
-/* eslint-disable no-use-before-define */
 /* eslint-disable linebreak-style */
 /* eslint-disable eqeqeq */
+/* eslint-disable no-use-before-define */
+/* eslint-disable linebreak-style */
 /* eslint-disable no-console */
+/* eslint-disable linebreak-style */
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const photographerId = urlParams.get('id');
@@ -19,24 +21,21 @@ fetch('./public/data.json')
   })
   .catch((error) => console.error(error));
 
-/* Dynamically add  the photographer to page */
+/* Dynamically add the photographer to page */
 const CreatePhotograperPage = (individual) => {
-/* declare a place to put the photographer profile in the dom */
-  const photographerProfile = document.querySelector('#main');
+/* declare a place to put the photographer profile & photo in the dom */
+  const photographerProfile = document.querySelector('.person-profile__details');
+  const photographerPhoto = document.querySelector('.person-profile__portraitPlaceholder');
   /* Using DESTRUCTERING get the photographer's tag array data */
   const { tags } = individual[0];
-  let photographerhtml = '';
 
-  photographerhtml += `
-  <article class="person-profile">
-   <div class="person-profile__container">
+  const photographerhtml = `
       <h2 class="person-profile__name">${individual[0].name}</h2>
       <h3 class="person-profile__address">${individual[0].city}, ${individual[0].country}</h3>
       <h4 class="person-profile__tagline">${individual[0].tagline}</h4>
-      <ul class="person-profile__tags" >${tags.map((tag) => `<li tabindex="0" class="tags" arial-label="${tag}">#${tag}</li>`).join('')}</ul>
-   </div>
-   <input type="button" value="Contactez-moi" class="person-profile__contactBtn">
-    <img class="person-profile__portrait" src="/public/images/photography/Photographers_id_photos/${individual[0].portrait}"alt="${individual[0].name}">
-</article>`;
+      <ul class="person-profile__tags" >${tags.map((tag) => `<li tabindex="0" class="tags" arial-label="${tag}">#${tag}</li>`).join('')}</ul>`;
   photographerProfile.innerHTML = photographerhtml;
+
+  const photographerPhotohtml = `<img class="person-profile__portrait" src="/public/images/photography/Photographers_id_photos/${individual[0].portrait}"alt="${individual[0].name}">`;
+  photographerPhoto.innerHTML = photographerPhotohtml;
 };
