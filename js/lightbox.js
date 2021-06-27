4bfef6af4f0f472f9bc72e052f77ae56272c6ddb
Mark Stevens <markspstevens@yahoo.co.uk>
2021-06-27 13:55:12 +0200

    basic lightbox working

--- js/lightbox.js
+++ js/lightbox.js
@@ -11,10 +11,21 @@ const lightboxModalbg = document.querySelector('.lightbox-modal');
 const closeLightboxModalBtn = document.querySelectorAll('.lightbox-modal__controls--close');
 
 // launch modal
-const launchLightboxModal = () => {
+const launchLightboxModal = (photographersMedia, i) => {
   mainWrapper.setAttribute('aria-hidden', 'true');
   lightboxModalbg.setAttribute('aria-hidden', 'false');
   lightboxModalbg.style.display = 'flex';
+
+  const lightboxElement = document.querySelector('#placeHolder');
+  let lightboxhtml = '';
+  if (photographersMedia[i].video) { /* if a video object return a video template */
+    lightboxhtml = `<video width="100%" controls class="lightbox-modal__media"><source type="video/mp4" src="/public/images/photography/${photographersMedia[i].photographerId}/${photographersMedia[i].video}">Sorry, your browser doesn't support embedded videos.</video>
+            <h2 class="lightbox-modal__title">${photographersMedia[i].title}</h2>`;
+  } else if (photographersMedia[i].image) { /* if an image object return an image template */
+    lightboxhtml = `<img class="lightbox-modal__media" src="/public/images/photography/${photographersMedia[i].photographerId}/${photographersMedia[i].image}" alt="${photographersMedia[i].title}">
+      <h2 class="lightbox-modal__title">${photographersMedia[i].title}</h2>`;
+  }
+  lightboxElement.innerHTML = lightboxhtml;
 };
 
 // close modal Lightbox modal
@@ -41,8 +52,8 @@ const lightbox = (photographersMedia) => {
     if (!event.target.matches('.artwork__image')) {
       return;
     }
-    console.log(findmapindexOf(parseInt(event.target.id, 10)));
-    launchLightboxModal();
+    const chosenMedia = findmapindexOf(parseInt(event.target.id, 10));
+    launchLightboxModal(photographersMedia, chosenMedia);
   });
 
   photoartworkElement.addEventListener('keyup', (event) => {
@@ -50,8 +61,8 @@ const lightbox = (photographersMedia) => {
       if (!event.target.matches('.artwork__image')) {
         return;
       }
-      console.log(findmapindexOf(parseInt(event.target.id, 10)));
-      launchLightboxModal();
+      const chosenMedia = findmapindexOf(parseInt(event.target.id, 10));
+      launchLightboxModal(photographersMedia, chosenMedia);
     }
   });
 };
